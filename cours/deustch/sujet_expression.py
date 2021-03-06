56630438389a05465f575fa9ea8c864ce0689684
centaurus <alexis.opolka@gmail.com>
2021-03-06 13:01:15 +0000

    Update file before trying Gitpod.io

--- cours/deustch/sujet_expression.py
+++ cours/deustch/sujet_expression.py
@@ -1,17 +1,36 @@
 ### Original Phrase:
 
-    """In seinem Interview sagt herr Schröder, dass 'man in denn Schulen und in den Medien viel mehr Fakten 
-    über das geteilte Deutschland vermitteln sollte.' Denken Sie auch, dass es wichtig ist, die Geschichte seines Landes zu kennen?
-    Begründne Sie Ihre Antwort mit konkreten beispielen."""
+    """
+        In seinem Interview sagt herr Schröder, dass 'man in denn Schulen und in den Medien viel mehr Fakten 
+        über das geteilte Deutschland vermitteln sollte.' Denken Sie auch, dass es wichtig ist, die Geschichte seines Landes zu kennen?
+        Begründne Sie Ihre Antwort mit konkreten beispielen.
+    """
 
 ### Approximative translation:
 
-    """In his interview, Mr. Schröder says that 'in schools and in the media, much more facts about the divided Germany should be present.' 
-    Do you also think that it is important to know the history of one's country?
-    Justify your answer with concrete examples."""
+    """
+        In his interview, Mr. Schröder says that 'in schools and in the media, much more facts about the divided Germany should be present.' 
+        Do you also think that it is important to know the history of one's country?
+        Justify your answer with concrete examples.
+    """
 
 ## Answer in German:
 
-    """Ich denke, dass es wichtig ist, die Geschichte die Menschheit zu kenne.
-    Es ist für nicht wiederholen zu fehler wie hatten in wir letzte
+    """
+        Ich denke, dass es wichtig ist, die Geschichte die Menschheit zu kennen.
+        Es ist für nicht wiederholen zu fehler wie hatten in wir letzte weil.
+    """
+
+## Answer in English:
+
+    """
+        I think it's important to preserve the history of mankind, it's mainly for trying not to repeat the same errors/problems we already passed through.
+        All the conflicts we had can serve us as a warning when we try to do somehting dangerous, a warning that dictates us what we should do and what we should not try to do.
+        What makes the history of mankind is the combination of every main events which happened in every country but the History, with a great 'H', isn't
+        really the same thing, it's the conglomerate of history of each country and everyone should be able to learn what happened in their own country.
+        
+        The past of the country someone lives in is a non-negligable part of its identity, how the citizens of a country reacts to an event or to a stranger or
+        a citizen of another country is influenced by the history they all share. 
+        If one day in their past there had a conflict between a certain country and their country and the outcome of it wasn't really comprehended or accepted
+        by them, they can surely still have grudges against the citizens of this country.
     """
