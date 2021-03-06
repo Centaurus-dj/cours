221d57768e167eff3e1b1c937591a4685e62e813
centaurus <alexis.opolka@gmail.com>
2021-03-06 14:47:12 +0000

    Gitpod configuration+final german work uploaded.

--- cours/deustch/sujet_expression.py
+++ cours/deustch/sujet_expression.py
@@ -18,19 +18,13 @@
 
     """
         Ich denke, dass es wichtig ist, die Geschichte die Menschheit zu kennen.
-        Es ist für nicht wiederholen zu fehler wie hatten in wir letzte weil.
-    """
-
-## Answer in English:
-
-    """
-        I think it's important to preserve the history of mankind, it's mainly for trying not to repeat the same errors/problems we already passed through.
-        All the conflicts we had can serve us as a warning when we try to do somehting dangerous, a warning that dictates us what we should do and what we should not try to do.
-        What makes the history of mankind is the combination of every main events which happened in every country but the History, with a great 'H', isn't
-        really the same thing, it's the conglomerate of history of each country and everyone should be able to learn what happened in their own country.
-        
-        The past of the country someone lives in is a non-negligable part of its identity, how the citizens of a country reacts to an event or to a stranger or
-        a citizen of another country is influenced by the history they all share. 
-        If one day in their past there had a conflict between a certain country and their country and the outcome of it wasn't really comprehended or accepted
-        by them, they can surely still have grudges against the citizens of this country.
+        Es ist für nicht wiederholen zu fehler wie hatten in wir letzte.
+        Alle Konflikten wir hatten kann warnung sein für wir zu meiden machen etwas gefährlich.
+        Deutschland und Frankreich sind freunds weil in letzte Sie Feinde sind und Sie haben Kriegs.
+        Sie sind fertig auf ein freundschaft weil Sie zwei Weltkrieg in ihrem Land haben und ihrem Bürger
+        wollten (veulent) Kriegs zu halten.
+        Nach die Zweiten Weltkrieg die Detuschland war in zwei getrennt aber im 1989 die Mauer zwischen
+        die zwei Staten ist gewesen vernicht, es ist die Berlinen Mauerfall und es ist weil die Berlinen und Deutschen
+        ihrem Geschichte konnst, dass Sie ein Wiedervereinigung wollten.
+        Unsere aktions sind die result (résultat) auf unsere Geschichte.
     """
