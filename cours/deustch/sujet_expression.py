390e75a9ca091bb51a382203ee72daa1ec3f2f0d
centaurus <alexis.opolka@gmail.com>
2021-03-06 13:01:26 +0100

    started import of german work

---
+++ cours/deustch/sujet_expression.py
@@ -0,0 +1,11 @@
+#Original Phrase:
+
+    "In seinem Interview sagt herr Schröder, dass 'man in denn Schulen und in den Medien viel mehr Fakten 
+    über das geteilte Deutschland vermitteln sollte.' Denken Sie auch, dass es wichtig ist, die Geschichte seines Landes zu kennen?
+    Begründne Sie Ihre Antwort mit konkreten beispielen."
+
+#Approximative translation:
+
+    "In his interview, Mr. Schröder says that 'in schools and in the media, much more facts about the divided 
+    about the divided Germany.' Do you also think that it is important to know the history of one's country?
+    Justify your answer with concrete examples."
