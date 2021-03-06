c6f6738f44250cb02e9436c3ce29d4d950f95b09
centaurus <alexis.opolka@gmail.com>
2021-03-06 12:33:04 +0000

    Update cours/deustch/sujet_expression.py

--- cours/deustch/sujet_expression.py
+++ cours/deustch/sujet_expression.py
@@ -1,11 +1,17 @@
-#Original Phrase:
+### Original Phrase:
 
-    "In seinem Interview sagt herr Schröder, dass 'man in denn Schulen und in den Medien viel mehr Fakten 
+    """In seinem Interview sagt herr Schröder, dass 'man in denn Schulen und in den Medien viel mehr Fakten 
     über das geteilte Deutschland vermitteln sollte.' Denken Sie auch, dass es wichtig ist, die Geschichte seines Landes zu kennen?
-    Begründne Sie Ihre Antwort mit konkreten beispielen."
+    Begründne Sie Ihre Antwort mit konkreten beispielen."""
 
-#Approximative translation:
+### Approximative translation:
 
-    "In his interview, Mr. Schröder says that 'in schools and in the media, much more facts about the divided 
-    about the divided Germany.' Do you also think that it is important to know the history of one's country?
-    Justify your answer with concrete examples."
+    """In his interview, Mr. Schröder says that 'in schools and in the media, much more facts about the divided Germany should be present.' 
+    Do you also think that it is important to know the history of one's country?
+    Justify your answer with concrete examples."""
+
+## Answer in German:
+
+    """Ich denke, dass es wichtig ist, die Geschichte die Menschheit zu kenne.
+    Es ist für nicht wiederholen zu fehler wie hatten in wir letzte
+    """
