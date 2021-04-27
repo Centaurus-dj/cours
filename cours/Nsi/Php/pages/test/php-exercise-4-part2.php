202d845fd6e4811fc3b925a4a058f402fda85665
Centaurus-dj <53085471+Centaurus-dj@users.noreply.github.com>
2021-04-27 13:43:21 +0200

    Update php-exercise-4-part2.php

--- cours/Nsi/Php/pages/test/php-exercise-4-part2.php
+++ cours/Nsi/Php/pages/test/php-exercise-4-part2.php
@@ -11,25 +11,26 @@
 
 <body class="dark-bg">
   <div id="get-back">
-    <a class="back-link" href="../index.php">Return</a>
+    <a class="back-link" href="../index.php">Return to index</a> <br>
+    <a class="back-link" href="php-exercise-4.php">Return to sender</a>
   </div> <br>
   <h1> PHP - Exercice 4 Part 2</h1>
   <?php
-  function controlSet($var, $type_request)
+  function controlSet($var, $requestType)
   {
-    if ($type_request == "get") {
+    if ($requestType == "get") {
       if (isset($_GET[$var])) {
         return $_GET[$var];
       } else {
         return null;
       }
-    } else if ($type_request == "post") {
+    } else if ($requestType == "post") {
       if (isset($_POST[$var])) {
         return $_POST[$var];
       } else {
         return null;
       }
-    } else if ($type_request == "both") {
+    } else if ($requestType == "both") {
       if (isset($_GET[$var])) {
         return $_GET[$var];
       } else if (isset($_POST[$var])) {
@@ -49,7 +50,7 @@
   $oget = controlSet("operation", "get");
   // POST METHOD
   $fpost = controlSet("nbr", "post");
-  $spost = controlSet("nb2", "post");
+  $spost = controlSet("nbr2", "post");
   $opost = controlSet("operation", "post");
 
   if ($method == "GET" | $method == "get") { //TODO: Work over variables scope to find a way to optimize this.
@@ -92,20 +93,31 @@
         echo "<p>le quotient de $fpost et $spost est $result</p>";
       }
     } else {
-      echo "<p>Something went wrong, please report this incident.</p>";
+      $is1 = isset($fpost);
+      $is2 = isset($spost);
+      $is3 = isset($opost);
+
+      echo "<p>Something went wrong, please report this incident.</p>
+      <div id=\"php-log\">
+        <txt>tested values:</txt>
+        <ul>
+          <elem>method: $method</elem>
+          <elem>fpost: $fpost</elem>
+          <elem>spost: $spost</elem>
+          <elem>opost: $opost</elem>
+        </ul>
+        <txt>conditions:</txt>
+        <ul>
+          <elem>is1: $is1</elem>
+          <elem>is2: $is2</elem>
+          <elem>is3: $is3</elem>
+        </ul>
+      </div>";
     }
   } else {
     echo "
-      <form action=\"php-exercise-4-part2.php\" method=\"post\">
-      <label for=\"nbr\">Nombre n°1:</label>
-      <input type=\"text\" name=\"nbr\" value=\"5\" onclick=\"document.getElementsByName('nbr')[0].value = '';\">
-      <div> et </div>
-      <label for=\"nbr2\">Nombre n°2:</label>
-      <input type=\"text\" name=\"nbr2\" value=\"7\" onclick=\"document.getElementsByName('nbr2')[0].value = '';\"> <br>
-      <label for=\"operation\">Opération à éffectuer:</label>
-      <input type=\"text\" name=\"operation\" value=\"+\" onclick=\"document.getElementsByName('operation')[0].value = '';\"><button onclick=\"operations = ['+', '-', '*', '/']; document.getElementsByName('operation')[0].value = operations[Math.floor(Math.random() * operations.length)];\"> Choisir aléatoirement l'opération</button> <br>
-      <input type=\"submit\" value=\"Valider\">
-      </form>";
+    <h1>Oh! Something went wrong, we couldn't determine which method the method is.</h1>
+    ";
   }
   ?>
 </body>
