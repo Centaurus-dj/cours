f4e89bf490c88cbaaec294d473e35308e5fc0340
centaurus <alexis.opolka@gmail.com>
2021-04-27 09:27:09 +0200

    Added docs + started from scracth WebServer using NodeJs

--- fedora-install-php.sh
+++ fedora-install-php.sh
@@ -1,1 +1,9 @@
+#! bin/bash
+
 sudo dnf install libxml2-devel.x86_64
+sudo dnf install libsqlite3x-devel.x86_64
+cd libs/php/linux/
+./configure
+make -j4
+sudo make install
+cd ../../../
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var http = require("http"),
   path = require("path"),
   fs = require("fs"),
   os = require("os"),
-port = process.argv[2] || 3000;
+  port = process.argv[2] || 3000;
 
-var { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
+const { extname } = require("path");
+
+var executed_times = 0;
 
 http.createServer(function (request, response) {
 
@@ -17,96 +20,37 @@ http.createServer(function (request, response) {
     '.css': "text/css",
     '.js': "text/javascript",
     '.pdf': "application/pdf",
+    '.php': "text/html"
   };
-  var serverLanguages = [
-    '.php',
-  ];
-
-
-  console.log("serving:", filename);
-
-  fs.exists(filename, function (exists) {
-
-    function isServerLanguage(extension) {
-    for (var element of serverLanguages) {
-      if (element == extension) {
-        return true;
-      }
+  var serverLanguages = {
+    '.html': false,
+    '.css': false,
+    '.js': false,
+    '.pdf': false,
+    '.php': true,
+  };
+  try {
+    if (fs.statSync(filename).isDirectory()) {
+      console.log("directory touched");
+      filename += '/index.html';
     }
-    return false;
-  }
-
-    // If the URL doesn't gives a file
-    if (!exists) {
-      response.writeHead(404, { "Content-Type": "text/html" });
-      response.write("<html><style>html{background:#1a1a1a;color:#fff;font-size:48px;user-select:none;text-align:center;}</style>404 not found</html>");
+      response.writeHead(200, contentTypesByExtension[path.extname(filename)]);
+      response.write(
+        '<html><body style="background-color:#1a1a1a; color: #fff; font-size: 28px;">It seems to run fine!</body></html>',
+        "utf-8"
+      );
       response.end();
-      return;
-    }
-
-    // If URL finish with "/" we search the index HTML file and send it.
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    // If URL states an existing file in dir
-    fs.readFile(filename, "binary", function (err, file) {
-      if (err) { // If when tries to reach file error occurs, send errors as response
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-
-      var headers = {};
-      var extname = path.extname(filename);
-      var contentType = contentTypesByExtension[extname];
-      console.log('LOG: ' + extname + ", " + isServerLanguage(extname) + ", " + isServerLanguage(".php"));
-
-
-      // If file needs to be processed by server then sent to client
-      if (isServerLanguage(extname)) {
-        console.log('Processing file...');
-        if (contentType) headers["Content-Type"] = contentType;
-
-        response.writeHead(200, headers);
-        if (os.platform == "win32") { // Script on Windows
-          console.log("LOG: Windows");
-          response.write(exec("libs/php/Win_php/php.exe" + filename, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-          }), "binary");
-
-        } else if (os.platform == "linux") { // Script on Linux
-          console.log("LOG: Linux");
-          exec("libs/php/Linux_php/php " + filename, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            response.write(stdout, "binary");
-          });
-        }
-        response.end();
-      } else {
-        console.log('content:', contentTypesByExtension[path.extname(filename)]);
-        if (contentType) headers["Content-Type"] = contentType;
-        response.writeHead(200, headers);
-        response.write(file, "binary");
-        response.end();
-      }
-    });
-  });
+  }
+  finally {
+    console.log('Something unexpected happened, we stop server.');
+    response.writeHead(502, 'text/html');
+    response.write(
+      '<html><body style="background-color:#1a1a1a; color: #fff; font-size: 28px;">Ah! Something went wrong.</body></html>',
+      "utf-8"
+    );
+    response.end();
+    return;
+  }
 }).listen(parseInt(port, 10));
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
