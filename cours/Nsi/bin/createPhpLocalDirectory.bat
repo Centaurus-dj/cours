d3ea675e229c2ea744736c1bf20dc3ad651cca93
Centaurus-dj <alexis.opolka@gmail.com>
2021-05-12 00:55:18 +0200

    Update files + rewrote .bat executioner

--- cours/Nsi/bin/createPhpLocalDirectory.bat
+++ cours/Nsi/bin/createPhpLocalDirectory.bat
@@ -1,10 +1,11 @@
 echo off
 
-mkdir "Travail-PHP-Alexis-Opolka-1G02" && cd "Travail-PHP-Alexis-Opolka-1G02"
-test.txt > Hello World
-test.txt >> End Line
+mkdir "Travail-PHP-Alexis-Opolka-1G02" & cd "Travail-PHP-Alexis-Opolka-1G02"
+echo Hello World> test.txt
+echo End Line>> test.txt
 
-Delete-Directory.bat > echo off
-Delete-Directory.bat >> echo "Deleting directory..."
-Delete-Directory.bat >> del "test.txt"
-Delete-Directory.bat >> echo "Directory deleted!"
+echo echo off> Delete-Directory.bat
+echo echo Deleting directory...>> Delete-Directory.bat
+echo cd ../>> Delete-Directory.bat
+echo del "test.txt">> Delete-Directory.bat
+echo echo Directory deleted!>> Delete-Directory.bat
